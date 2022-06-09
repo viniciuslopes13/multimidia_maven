6e382a3dbb0ff9c99da4022db29c22d416d2afa1
viniciuslopes13 <vinicius.lopes.c@hotmail.com>
2022-06-09 14:20:55 -0300

    projeto concluído

--- src/main/java/br/com/multimidia/app/Filtro.java
+++ src/main/java/br/com/multimidia/app/Filtro.java
@@ -5,29 +5,21 @@ import java.awt.image.BufferedImage;
 
 public class Filtro {
 
-	// m�todo de aplica��o do filtro escala de cinza
-	// recebe como par�metro uma imagem
 	public static BufferedImage escalaDeCinza(BufferedImage imagem) {
-		// pegar largura e altura da imagem
+
 		int width = imagem.getWidth();
 		int height = imagem.getHeight();
 
 		int media = 0;
-		// la�o para varrer a matriz de pixels da imagem
 		for (int i = 0; i < width; i++) {
-			for (int j = 0; j < height; j++) { // rgb recebe o valor RGB do pixel em quest�o
+			for (int j = 0; j < height; j++) { 
 				int rgb = imagem.getRGB(i, j);
 				int r = (int) ((rgb & 0x00FF0000) >>> 16); // R
 				int g = (int) ((rgb & 0x0000FF00) >>> 8); // G
 				int b = (int) (rgb & 0x000000FF); // B
 
-				// media dos valores do RGB
-				// ser� o valor do pixel na nova imagem
 				media = (r + g + b) / 3;
-
-				// criar uma inst�ncia de Color
 				Color color = new Color(media, media, media);
-				// setar o valor do pixel com a nova cor
 				imagem.setRGB(i, j, color.getRGB());
 			}
 		}
@@ -39,7 +31,7 @@ public class Filtro {
 		int height = image.getHeight();
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
-				int rgb = image.getRGB(i, j); // a cor inversa � dado por 255 menos o valor da cor
+				int rgb = image.getRGB(i, j); 
 				int r = 255 - (int) ((rgb & 0x00FF0000) >>> 16);
 				int g = 255 - (int) ((rgb & 0x0000FF00) >>> 8);
 				int b = 255 - (int) (rgb & 0x000000FF);
@@ -62,10 +54,6 @@ public class Filtro {
 				int media = (r + g + b) / 3;
 				Color white = new Color(255, 255, 255);
 				Color black = new Color(0, 0, 0);
-				// como explicado no artigo, no threshold definimos um limiar,
-				// que � um valor "divisor de �guas"
-				// pixels com valor ABAIXO do limiar viram pixels PRETOS,
-				// pixels com valor ACIMA do limiar viram pixels BRANCOS
 				if (media < limiar)
 					image.setRGB(i, j, black.getRGB());
 				else
--- src/main/java/br/com/multimidia/app/UploadMBean.java
+++ src/main/java/br/com/multimidia/app/UploadMBean.java
@@ -1,16 +1,22 @@
 package br.com.multimidia.app;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.imageio.ImageIO;
 
 import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 import org.primefaces.model.file.UploadedFile;
 import org.primefaces.model.file.UploadedFileWrapper;
 
@@ -20,20 +26,53 @@ import lombok.Setter;
 @Getter
 @Setter
 @ManagedBean
-@RequestScoped
+@SessionScoped
 public class UploadMBean {
 	
 	private Boolean flag;
 	private UploadedFile uploadedFile;
+	private StreamedContent streamedContent;
 	private String tipoFiltro;
 	
 	public UploadMBean() {
 		flag = true;
 		uploadedFile = new UploadedFileWrapper();
+		streamedContent = new DefaultStreamedContent();
 	}
 	
-	public void download() {
-		System.out.println(tipoFiltro);
+	public void downloadArquivo() throws IOException {
+		flag =true;
+		String dir = "C:\\Users\\Usuário\\eclipse-workspace\\multimida_maven\\";
+		
+		//Carrega Imagem
+		BufferedImage img = ImageIO.read(new File(dir+uploadedFile.getFileName()));
+		
+		/*Instancia filtro*/
+		Filtro filtro = new Filtro();
+		
+		/*Aplica filtro conforme opção do usuário*/
+		if(tipoFiltro.equals("1")) {
+			filtro.escalaDeCinza(img);
+		}else if(tipoFiltro.equals("2")){
+			filtro.negativo(img);
+		}else {
+			filtro.threshold(img, 200);
+		}
+		
+		/*Gera imagem com filtro*/
+		ImageIO.write(img,"png",new File(dir+"imagemGerada.png"));
+		
+		/*Faz o download da imagem filtrada*/
+		InputStream in = new FileInputStream(new File(dir+"imagemGerada.png"));
+		streamedContent = DefaultStreamedContent.builder()
+				.name("download.png")
+				.contentType(uploadedFile.getContentType())
+				.stream(()->in)
+				.build();
+		
+		/*Mensagem de sucesso*/
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage("Download", "Download concluído!"));
 	}
 
 	public void upload(FileUploadEvent event) {
@@ -48,7 +87,7 @@ public class UploadMBean {
 			out.close();
 
 			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage("Upload completo", "O arquivo " + uploadedFile.getFileName() + " foi salvo!"));
+					new FacesMessage("Upload completo", "O arquivo " + uploadedFile.getFileName() + " foi enviado!"));
 		} catch (IOException e) {
 			FacesContext.getCurrentInstance().addMessage(null,
 					new FacesMessage(FacesMessage.SEVERITY_WARN, "Erro", e.getMessage()));
